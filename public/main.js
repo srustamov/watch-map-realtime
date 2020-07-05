71ad6cb6f43d69649b00c65a8bf9b9f2c9c6a4cf
srustamov <rustemovv96@gmail.com>
2020-07-05 13:32:31 +0400

    '.'

--- public/main.js
+++ public/main.js
@@ -1,5 +1,7 @@
 "use strict"
 
+var markers = [];
+
 
 const map = L.map('map').setView([40.3963904, 49.9187712], 10);
 
@@ -8,7 +10,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
-let markers = [];
 
 window.onload = () => {
 
@@ -24,50 +25,51 @@ window.onload = () => {
         clearMarker(id)
     });
 
-    const getCoordFromData = (data) => {
-        return [data.latitude, data.longitude].map(c => parseFloat(c));
+}
+
+const getCoordFromData = (data) => {
+    return [data.latitude, data.longitude].map(c => parseFloat(c));
+}
+
+
+const createOrUpdateMarker = (data) => {
+
+    let popUpContent = `
+    imei: ${data.imei} <br/>
+    serial: ${data.serial} <br/>
+    latitude: ${data.latitude} <br/>
+    latitude: ${data.longitude} <br/>
+    altitude: ${data.altitude} <br/>
+    time: ${data.time} <br/>
+`;
+
+    let marker = markers.find(m => m._id === data.socket_id);
+
+    if (marker) {
+        marker.setLatLng(getCoordFromData(data)).update();
+        marker.setPopupContent(popUpContent)
+    } else {
+
+        let marker = L.marker(getCoordFromData(data)).addTo(map)
+            .bindPopup(popUpContent);
+        marker._id = data.socket_id;
+
+        markers.push(marker);
     }
+}
 
 
-    const createOrUpdateMarker = (data) => {
+const clearMarker = (id) => {
 
-        let popUpContent = `
-        imei: ${data.imei} <br/>
-        serial: ${data.serial} <br/>
-        latitude: ${data.latitude} <br/>
-        latitude: ${data.longitude} <br/>
-        altitude: ${data.altitude} <br/>
-        time: ${data.time} <br/>
-    `;
+    let updated = [];
 
-        let marker = markers.find(m => m._id === data.socket_id);
-
-        if (marker) {
-            marker.setLatLng(getCoordFromData(data)).update();
-            marker.setPopupContent(popUpContent)
+    markers.forEach((marker) => {
+        if (marker._id === id) {
+            map.removeLayer(marker)
         } else {
-
-            let marker = L.marker(getCoordFromData(data)).addTo(map)
-                .bindPopup(popUpContent);
-            marker._id = data.socket_id;
-
-            markers.push(marker);
+            updated.push(marker);
         }
-    }
+    });
 
-
-    const clearMarker = (id) => {
-
-        let updated = [];
-
-        markers.forEach((marker) => {
-            if (marker._id === id) {
-                map.removeLayer(marker)
-            } else {
-                updated.push(marker);
-            }
-        });
-
-        markers = updated;
-    }
+    markers = updated;
 }
