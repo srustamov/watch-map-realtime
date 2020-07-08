ee912b23951aac24f8c98531e0910b9708923d6a
srustamov <rustemovv96@gmail>
2020-07-08 17:36:39 +0400

    .

--- public/main.js
+++ public/main.js
@@ -2,43 +2,54 @@
 
 const PI = 3.14159265;
 const TWO_PI = 2 * PI;
-
-var markers = [];
-
-const polygons = [
-  {
-    name: "AirPort",
-    latitudes: [
-      40.465243,
-      40.466957,
-      40.466812,
-      40.464944,
-      40.461626,
-      40.459649,
-    ],
-    longitudes: [
-      50.050168,
-      50.052108,
-      50.053873,
-      50.054311,
-      50.054544,
-      50.052609,
-    ],
-  },
-];
-
 const polygonCoords = [];
 
+var markers = [];
+// 40.46345409436718, 50.04817485809327
+const polygons = [{
+        name: "AirPort",
+        latitudes: [
+            40.465243,
+            40.466957,
+            40.466812,
+            40.464944,
+            40.461626,
+            40.459649,
+            // 40.46345409436718,
+        ],
+        longitudes: [
+            50.050168,
+            50.052108,
+            50.053873,
+            50.054311,
+            50.054544,
+            50.052609,
+            // 50.04817485809327,
+        ],
+    },
+    // {
+    //     name: "test",
+    //     latitudes: [
+    //         40.4632581889531,
+    //         40.46833521978792,
+    //     ],
+    //     longitudes: [
+    //         50.049483776092536,
+    //         50.04008531570435
+    //     ]
+    // },
+];
+
 polygons.forEach((polygon) => {
-  polygon.latitudes.forEach((latitude, index) => {
-    polygonCoords.push([latitude, polygon.longitudes[index]]);
-  });
+    polygon.latitudes.forEach((latitude, index) => {
+        polygonCoords.push([latitude, polygon.longitudes[index]]);
+    });
 });
 
 const map = L.map("map").setView([40.462964, 50.052201], 15);
 
-map.on("click", function (e) {
-  console.log("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng);
+map.on("click", function(e) {
+    console.log("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng);
 });
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -46,35 +57,28 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 window.onload = () => {
-<<<<<<< HEAD
+    initPolygons();
+
     const socket = io();
 
     socket.on("location", (data) => {
         if (data.socket_id) {
+            let isSafeArea = ValidateCoords.isInsidePitch(
+                data.latitude,
+                data.longitude,
+                polygons[0].latitudes,
+                polygons[0].longitudes
+            );
+            if (isSafeArea) {
+                socket.emit("safe-area", {
+                    id: data.socket_id,
+                    name: polygons[0].name,
+                });
+                notifyMe("Safe Area");
+            }
             createOrUpdateMarker(data);
         }
     });
-=======
-  initPolygons();
-
-  const socket = io();
-
-  socket.on("location", (data) => {
-    if (data.socket_id) {
-      let isSafeArea = ValidateCoords.isInsidePitch(
-        data.latitude,
-        data.longitude,
-        polygons[0].latitudes,
-        polygons[0].longitudes
-      );
-      if (isSafeArea) {
-        socket.emit('safe-area',{id:data.socket_id,name:polygons[0].name})
-        notifyMe("Safe Area");
-      }
-      createOrUpdateMarker(data);
-    }
-  });
->>>>>>> 6c6d8c76a1d0caca1687708b2c218ba8f75d13c9
 
     socket.on("leave", (id) => {
         clearMarker(id);
@@ -82,11 +86,7 @@ window.onload = () => {
 };
 
 const getCoordsFromData = (data) => {
-<<<<<<< HEAD
     return [data.latitude, data.longitude].map((c) => parseFloat(c));
-=======
-  return [data.latitude, data.longitude].map((c) => parseFloat(c));
->>>>>>> 6c6d8c76a1d0caca1687708b2c218ba8f75d13c9
 };
 
 const createOrUpdateMarker = (data) => {
@@ -99,7 +99,6 @@ const createOrUpdateMarker = (data) => {
 
     let marker = markers.find((m) => m._id === data.socket_id);
 
-<<<<<<< HEAD
     if (marker) {
         marker.setLatLng(getCoordsFromData(data)).update();
         marker.setPopupContent(popUpContent);
@@ -108,23 +107,11 @@ const createOrUpdateMarker = (data) => {
             .addTo(map)
             .bindPopup(popUpContent);
         marker._id = data.socket_id;
-=======
-  if (marker) {
-    marker.setLatLng(getCoordsFromData(data)).update();
-    marker.setPopupContent(popUpContent);
-  } else {
-    let marker = L.marker(getCoordsFromData(data))
-      .addTo(map)
-      .bindPopup(popUpContent);
-    marker._id = data.socket_id;
->>>>>>> 6c6d8c76a1d0caca1687708b2c218ba8f75d13c9
-
         markers.push(marker);
     }
 };
 
 const clearMarker = (id) => {
-<<<<<<< HEAD
     let index = markers.findIndex((marker) => marker._id === id);
 
     if (index > -1) {
@@ -132,6 +119,97 @@ const clearMarker = (id) => {
         markers.splice(index, 1);
     }
 };
+
+const initPolygons = () => {
+    let polygon = L.polygon([
+        polygonCoords, [
+            [40.4632581889531, 50.049483776092536],
+            [40.46833521978792, 50.04008531570435],
+        ]
+    ], {
+        color: "#ff7800",
+        weight: 3,
+    }).addTo(map);
+
+    L.polylineDecorator(polygon, {
+        patterns: [{
+            offset: 0,
+            repeat: 10,
+            symbol: L.Symbol.dash({
+                pixelSize: 0,
+            }),
+        }, ],
+    }).addTo(map);
+};
+
+const notifyMe = (message) => {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+    }
+
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+        // If it's okay let's create a notification
+        var notification = new Notification("Hi there!");
+    }
+
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== "denied") {
+        Notification.requestPermission().then(function(permission) {
+            // If the user accepts, let's create a notification
+            if (permission === "granted") {
+                var notification = new Notification(message);
+            }
+        });
+    }
+};
+
+class ValidateCoords {
+    static isInsidePitch = (latitude, longitude, latArray, longArray) => {
+        let angle = 0;
+        let p1Lat;
+        let p1Long;
+        let p2Lat;
+        let p2Long;
+        let n = latArray.length;
+
+        for (let i = 0; i < n; i++) {
+            p1Lat = latArray[i] - latitude;
+            p1Long = longArray[i] - longitude;
+            p2Lat = latArray[(i + 1) % n] - latitude;
+            p2Long = longArray[(i + 1) % n] - longitude;
+            angle += ValidateCoords.angle2D(p1Lat, p1Long, p2Lat, p2Long);
+        }
+
+        return !(Math.abs(angle) < PI);
+    };
+
+    static angle2D = (y1, x1, y2, x2) => {
+        let dtheta, theta1, theta2;
+
+        theta1 = Math.atan2(y1, x1);
+        theta2 = Math.atan2(y2, x2);
+        dtheta = theta2 - theta1;
+        while (dtheta > PI) dtheta -= TWO_PI;
+        while (dtheta < -PI) dtheta += TWO_PI;
+
+        return dtheta;
+    };
+
+    static isValidCoordinate = (latitude, longitude) => {
+        return (
+            latitude !== "" &&
+            longitude !== "" &&
+            !isNaN(latitude) &&
+            !isNaN(longitude) &&
+            latitude > -90 &&
+            latitude < 90 &&
+            longitude > -180 &&
+            longitude < 180
+        );
+    };
+}
 
 class Marker {
     centerCoords = [40.3963904, 49.9187712];
@@ -224,192 +302,3 @@ class Marker {
         }
     };
 }
-=======
-  let index = markers.findIndex((marker) => marker._id === id);
-
-  if (index > -1) {
-    map.removeLayer(markers[index]);
-    markers.splice(index, 1);
-  }
-};
-
-const initPolygons = () => {
-  let polygon = L.polygon([polygonCoords], {
-    color: "#ff7800",
-    weight: 3,
-  }).addTo(map);
-
-  L.polylineDecorator(polygon, {
-    patterns: [
-      {
-        offset: 0,
-        repeat: 10,
-        symbol: L.Symbol.dash({
-          pixelSize: 0,
-        }),
-      },
-    ],
-  }).addTo(map);
-};
-
-const notifyMe = (message) => {
-  // Let's check if the browser supports notifications
-  if (!("Notification" in window)) {
-    alert("This browser does not support desktop notification");
-  }
-
-  // Let's check whether notification permissions have already been granted
-  else if (Notification.permission === "granted") {
-    // If it's okay let's create a notification
-    var notification = new Notification("Hi there!");
-  }
-
-  // Otherwise, we need to ask the user for permission
-  else if (Notification.permission !== "denied") {
-    Notification.requestPermission().then(function (permission) {
-      // If the user accepts, let's create a notification
-      if (permission === "granted") {
-        var notification = new Notification(message);
-      }
-    });
-  }
-};
-
-class ValidateCoords {
-  static isInsidePitch = (latitude, longitude, latArray, longArray) => {
-    let angle = 0;
-    let p1Lat;
-    let p1Long;
-    let p2Lat;
-    let p2Long;
-    let n = latArray.length;
-
-    for (let i = 0; i < n; i++) {
-      p1Lat = latArray[i] - latitude;
-      p1Long = longArray[i] - longitude;
-      p2Lat = latArray[(i + 1) % n] - latitude;
-      p2Long = longArray[(i + 1) % n] - longitude;
-      angle += ValidateCoords.angle2D(p1Lat, p1Long, p2Lat, p2Long);
-    }
-
-    return !(Math.abs(angle) < PI);
-  };
-
-  static angle2D = (y1, x1, y2, x2) => {
-    let dtheta, theta1, theta2;
-
-    theta1 = Math.atan2(y1, x1);
-    theta2 = Math.atan2(y2, x2);
-    dtheta = theta2 - theta1;
-    while (dtheta > PI) dtheta -= TWO_PI;
-    while (dtheta < -PI) dtheta += TWO_PI;
-
-    return dtheta;
-  };
-
-  static isValidCoordinate = (latitude, longitude) => {
-    return (
-      latitude !== "" &&
-      longitude !== "" &&
-      !isNaN(latitude) &&
-      !isNaN(longitude) &&
-      latitude > -90 &&
-      latitude < 90 &&
-      longitude > -180 &&
-      longitude < 180
-    );
-  };
-}
-
-class Marker {
-  centerCoords = [40.3963904, 49.9187712];
-
-  zoom = 10;
-
-  markers = [];
-
-  setBuilder = (builder) => {
-    this.builder = builder;
-
-    return this;
-  };
-
-  setZoom = (zoom) => {
-    this.zoom = zoom;
-
-    return this;
-  };
-
-  setCenter = (coords) => {
-    this.centerCoords = coords;
-
-    return this;
-  };
-
-  init = (el) => {
-    this.map = this.builder.map(el).setView(this.centerCoords, this.zoom);
-    this.builder
-      .tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution: "&copy; Samir Rustamov",
-      })
-      .addTo(this.map);
-
-    return this;
-  };
-
-  getCoordsFromData = (data) => {
-    return [data.latitude, data.longitude].map((c) => parseFloat(c));
-  };
-
-  createPopUp = (data) => {
-    return `
-        imei: ${data.imei} <br/>
-        serial: ${data.serial} <br/>
-        latitude: ${data.latitude} <br/>
-        longitude: ${data.longitude} <br/>
-        altitude: ${data.altitude} <br/>`;
-  };
-
-  create = () => {
-    let marker = this.builder
-      .marker(this.getCoordsFromData(data))
-      .addTo(this.map)
-      .bindPopup(this.createPopUp(data));
-    marker._id = data.socket_id;
-
-    this.markers.push(marker);
-  };
-
-  update = (marker, data) => {
-    marker.setLatLng(this.getCoordsFromData(data)).update();
-    marker.setPopupContent(this.createPopUp(data));
-  };
-
-  createOrUpdate = (data) => {
-    let popUpContent = this.createPopUp(data);
-
-    let marker = this.markers.find((m) => m._id === data.socket_id);
-
-    if (marker) {
-      marker.setLatLng(this.getCoordsFromData(data)).update();
-      marker.setPopupContent(popUpContent);
-    } else {
-      let marker = L.marker(this.getCoordsFromData(data))
-        .addTo(this.map)
-        .bindPopup(popUpContent);
-      marker._id = data.socket_id;
-
-      this.markers.push(marker);
-    }
-  };
-
-  clear = (id) => {
-    let index = this.markers.findIndex((marker) => marker._id === id);
-
-    if (index > -1) {
-      this.map.removeLayer(this.markers[index]);
-      this.markers.splice(index, 1);
-    }
-  };
-}
->>>>>>> 6c6d8c76a1d0caca1687708b2c218ba8f75d13c9
