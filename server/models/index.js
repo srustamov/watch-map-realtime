9aac871fe193a00365572418ab0de2b4e1c239af
srustamov <rustemovv96@gmail>
2020-07-19 19:21:17 +0400

    .

--- server/models/index.js
+++ server/models/index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const config = require("../configs/database.js");
 
 const Sequelize = require("sequelize");
+
 const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
     host: config.HOST,
     dialect: config.dialect,
